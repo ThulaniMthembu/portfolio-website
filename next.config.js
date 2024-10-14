e6efdd6aead7b8c387601a3bf5574db70cbc05c9
thulani <thulanim457@gmail.com>
2024-10-14 16:29:25 +0200

    Verification fixes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
 		unoptimized: true,
 	},
 	output: 'export',
+	async headers() {
+		return [
+			{
+				source: '/google523f418b1738107c.html',
+				headers: [
+					{
+						key: 'Content-Type',
+						value: 'text/html',
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
