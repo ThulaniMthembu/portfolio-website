bc65e14b15ceff465687d283a9ddc53f770bc221
thulani <thulanim457@gmail.com>
2024-11-07 18:14:34 +0200

    resume updates

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -40,7 +40,7 @@ const projects = [
     description: "Amare Swimwear is a modern e-commerce platform built with Next.js, React, and TypeScript, offering a responsive design, secure user authentication, dynamic product catalog, and seamless checkout process for luxury swimwear enthusiasts.",
     image: "/images/amare-lp.png",
     tags: ["Next.js", "React", "TypeScript", "Vercel", "Firebase", "EmailJS"],
-    link: "https://amareswimwear.vercel.app/",
+    link: "https://swimwearbyamare.vercel.app/",
     date: "2024-10-01",
     type: "E-commerce"
   },
